157963cbe3f630ff2202264d6ea41d1c9a0db98a
luckyy777 <olli3tayl0r2003@gmail.com>
2022-12-06 15:03:56 +0000

    first commit

---
+++ blocks.h
@@ -0,0 +1,18 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon, Command, Update Interval, Update Signal*/
+
+	{"", "bash ~/.local/bin/statusbar/music.sh", 1, 20},
+
+	{"", "bash ~/.local/bin/statusbar/brightness.sh", 1, 30},
+
+	{"", "bash ~/.local/bin/statusbar/battery.sh", 1, 0},
+
+	{"", "bash ~/.local/bin/statusbar/volume.sh", 1, 10},
+
+	{"", "bash ~/.local/bin/statusbar/datetime.sh", 1, 0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
